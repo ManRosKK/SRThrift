715548a2c29222b3f19237e25df5dbaa11e2cf1f
kazelot91 <kazelot91@gmail.com>
2014-12-26 19:35:31 +0100

    projekt CS

---
+++ Projects/BankingNode/BankingNode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Thrift;
+using SRBanking;
+namespace BankingNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
