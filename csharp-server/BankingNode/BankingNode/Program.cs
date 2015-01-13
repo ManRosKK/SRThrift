49c32971bc5ccc4b5afc0c73602f4a1b88618fa9
kazelot91 <kazelot91@gmail.com>
2015-01-13 01:21:11 +0100

    -Parsowanie Ini

--- csharp-server/BankingNode/BankingNode/Program.cs
+++ csharp-server/BankingNode/BankingNode/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using Thrift;
 using SRBanking;
+using Ini;
 namespace BankingNode
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
